7b3b468557196d6d40752abf4244749594b46363
saldanajg4 <saldanajg4@hotmail.com>
2020-07-30 15:24:52 -0500

    Added cmdbuser

---
+++ src/CommandAPI/Migrations/20200730170447_CmdUserAdded3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommandAPI.Migrations
+{
+    public partial class CmdUserAdded3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
